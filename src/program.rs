45531187442e682d70198d98f6a98d9e6fc6a448
Lúcás Críostóir Meier <lucas@cronokirby.com>
2022-04-23 21:59:41 +0200

    Add serialization for operations and programs
    This will be necessary later, since we want to commit to the program in the proof.

--- src/program.rs
+++ src/program.rs
@@ -1,8 +1,10 @@
+use std::io;
+
 /// Represents an individual operation in the program.
 ///
 /// Each of these manipulates the program stack, potentially reading input.
 #[derive(Clone, Copy, Debug, PartialEq)]
-enum Operation {
+pub enum Operation {
     /// PUSH(!POP)
     Not,
     /// PUSH(POP & POP)
@@ -10,9 +12,28 @@ enum Operation {
     /// PUSH(POP ^ POP)
     Xor,
     /// Push an argument bit, with the right index.
-    PushArg(usize),
+    PushArg(u32),
     /// Push a local element, indexed from the bottom of the stack
-    PushLocal(usize)
+    PushLocal(u32),
+}
+
+impl Operation {
+    /// Serialize this operation into something implementing Write.
+    pub fn write_to(&self, w: &mut impl io::Write) -> io::Result<()> {
+        match self {
+            Operation::Not => w.write_all(&[0]),
+            Operation::And => w.write_all(&[1]),
+            Operation::Xor => w.write_all(&[2]),
+            Operation::PushArg(index) => {
+                w.write_all(&[3])?;
+                w.write_all(&index.to_le_bytes())
+            }
+            Operation::PushLocal(index) => {
+                w.write_all(&[4])?;
+                w.write_all(&index.to_le_bytes())
+            }
+        }
+    }
 }
 
 /// Represents a program, implementing some kind of boolean circuit.
@@ -32,5 +53,13 @@ impl Program {
         Self {
             operations: operations.into(),
         }
+    }
+
+    /// Serialize this program into something implementing Write.
+    pub fn write_to(&self, w: &mut impl io::Write) -> io::Result<()> {
+        for op in &self.operations {
+            op.write_to(w)?;
+        }
+        Ok(())
     }
 }
